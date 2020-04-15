0e89647dcf5bc6235d64b7d314a6b8c2d0385996
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-15 01:59:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,6 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN"
-
 end
 
 # Whatever you say to grandma, she should respond with
