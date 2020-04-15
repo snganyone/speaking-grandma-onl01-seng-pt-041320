3a0d4536db1cb480a0b203dee004b0fd637e62c5
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-15 02:27:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
-  if phrase == "I LOVE YOU GRANDMA!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
